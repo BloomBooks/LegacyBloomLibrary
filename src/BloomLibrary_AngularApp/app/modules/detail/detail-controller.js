9905287325d1bb76b9e70d6b7a7630a290781068
John Thomson <john_thomson@sil.org>
2013-11-07 15:26:51 -0600

    Make the detail dialog close when the back button is clicked.
    That is, the back button in the browser itself should close
    the dialog.

--- src/BloomLibrary_AngularApp/app/modules/detail/detail-controller.js
+++ src/BloomLibrary_AngularApp/app/modules/detail/detail-controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
-angular.module('BloomLibraryApp.detail').controller('DetailCtrl', ['$scope', '$state', '$stateParams', 'dialog', 'bookService',
+angular.module('BloomLibraryApp.detail').controller('DetailCtrl', ['$scope', '$state', '$stateParams', 'dialog', 'bookService', '$location',
 
-    function ($scope, $state, $stateParams, dialog, bookService) {
+	function ($scope, $state, $stateParams, dialog, bookService, $location) {
         //get the book for which we're going to show the details
         bookService.getBookById($stateParams.bookId).then( function(book){
             $scope.book = book;
@@ -11,6 +11,11 @@ angular.module('BloomLibraryApp.detail').controller('DetailCtrl', ['$scope', '$s
       $scope.close = function () {
           dialog.close();
       };
+
+	  // This is so the dialog closes when the back button in the browser is used.
+	  $scope.$on('$locationChangeSuccess', function(event) {
+		  dialog.close();
+	  });
 
       $scope.dowloand = function () {
           alert('download');
