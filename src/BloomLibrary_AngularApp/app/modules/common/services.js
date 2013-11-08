0973d4f359e9cb6e4375981c86c373294e2d7489
John Thomson <john_thomson@sil.org>
2013-11-08 14:15:51 -0600

    Get rid of 'default' config...we only have one config

--- src/BloomLibrary_AngularApp/app/modules/common/services.js
+++ src/BloomLibrary_AngularApp/app/modules/common/services.js
@@ -9,7 +9,7 @@ angular.module('BloomLibraryApp.services', ['restangular'])
 			'X-Parse-REST-API-Key':'P6dtPT5Hg8PmBCOxhyN9SPmaJ8W4DcckyW0EZkIx'
 		};
 
-        var restangularDefaultConfig = function (restangularConfigurer) {
+		var restangularConfig = function (restangularConfigurer) {
 			restangularConfigurer.setBaseUrl('https://api.parse.com/1');// 1/indicates rev 1 of parse.com API
             restangularConfigurer.setDefaultHeaders(headers);
         };
@@ -21,7 +21,7 @@ angular.module('BloomLibraryApp.services', ['restangular'])
 
 			isLoggedIn: function () { return isLoggedIn; },
 
-			defaultConfig: function () { return restangularDefaultConfig;},
+			config: function () { return restangularConfig;},
 
             setSession : function(sessionToken) {
 				var sessionTokenKey = 'X-Parse-Session-Token';
@@ -37,7 +37,7 @@ angular.module('BloomLibraryApp.services', ['restangular'])
 
             login: function(username, password, successCallback, errorCallback) {
                 // GET: .../login
-                restangular.withConfig(restangularDefaultConfig).all('login').getList({ 'username': username, 'password': password })
+				restangular.withConfig(restangularConfig).all('login').getList({ 'username': username, 'password': password })
                     .then(function (result) {
                         isLoggedIn = true;
                         userNameX = username;
@@ -63,12 +63,12 @@ angular.module('BloomLibraryApp.services', ['restangular'])
    
 	.service('bookService', ['Restangular', 'authService', function(restangular, authService) {
 		this.getAllBooks = function () {
-			return restangular.withConfig(authService.defaultConfig()).all('classes/books').getList({"limit":50}).then(function(resultWithWrapper)   {
+			return restangular.withConfig(authService.config()).all('classes/books').getList({"limit":50}).then(function(resultWithWrapper)   {
 		        return resultWithWrapper.results;
 		    })
 		};		
 		this.getBookById = function (id) {
-			return restangular.withConfig(authService.defaultConfig()).one('classes/books',id).get();
+			return restangular.withConfig(authService.config()).one('classes/books',id).get();
 		};
 	}])
 	.service('userService', ['Restangular', 'authService', function(restangular, authService) {
@@ -79,11 +79,11 @@ angular.module('BloomLibraryApp.services', ['restangular'])
 
 		this.register = function(user, callback) {
 			if (!user.mandatoryfield) {
-				return restangular.withConfig(authService.defaultConfig()).all('users').post(user).then(callback,callback);
+				return restangular.withConfig(authService.config()).all('users').post(user).then(callback,callback);
 			}
 		};
 		
 		this.readByUserName = function(username, callback) {
-			return restangular.withConfig(authService.defaultConfig()).all('users').getList({"where": '{"username": "' + username + '"}'}).then(callback,callback);
+			return restangular.withConfig(authService.config()).all('users').getList({"where": '{"username": "' + username + '"}'}).then(callback,callback);
 		};
 	}]);
