f328fb0748a851df26ab96eb5a049d9d188c646d
Andrew Polk <andrew_polk@sil.org>
2014-06-24 09:21:08 -0400

    Fix load of UserVoice widget -more improvements post-review
    Give script an id for direct reference.
    Improve comments.
    Make unique reference key a variable.

--- src/app/modules/suggestions/suggestions.js
+++ src/app/modules/suggestions/suggestions.js
@@ -17,19 +17,21 @@
                 // Include the UserVoice external script.  This is needed to load the embedded widget.
                 // To make sure we don't have multiple copies (which would happen if the user leaves and returns to the page),
                 // remove the script if it has already been added.  We have to re-add it or the widget will not be displayed.
-                // xoQAlIzNrOFQpMmMnRcg7w is the special reference for bloombooks.uservoice.com
-                var s = document.getElementsByTagName('script')[0];
-                if (s.src.indexOf('xoQAlIzNrOFQpMmMnRcg7w') > 0) {
+                // xoQAlIzNrOFQpMmMnRcg7w is the unique identifier for our Bloom feedback system on UserVoice (bloombooks.uservoice.com)
+                var uvReferenceKey = 'xoQAlIzNrOFQpMmMnRcg7w';
+                var s = document.getElementById('userVoiceScript');
+                if (s) {
                     s.parentNode.removeChild(s);
                 }
                 
-                // The remaining code is copy/paste from UserVoice.
+                // The remaining code is provided as is by UserVoice (other than adding an id and making uvReferenceKey a variable.
                 var uvOptions = {};
                 (function() {
-                    var uv = document.createElement('script'); 
+                    var uv = document.createElement('script');
+                    uv.id = 'userVoiceScript';
                     uv.type = 'text/javascript'; 
                     uv.async = true;
-                    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/xoQAlIzNrOFQpMmMnRcg7w.js';
+                    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/' + uvReferenceKey + '.js';
                     var s = document.getElementsByTagName('script')[0];
                     s.parentNode.insertBefore(uv, s);
                 })();
