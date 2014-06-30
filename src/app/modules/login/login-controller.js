42f713a13d61280d4c106bf8b9e1dac67d7feb3d
JohnThomson <john_thomson@sil.org>
2014-06-30 14:36:23 -0500

    Fix handling of query results to regain saving login status

--- src/app/modules/login/login-controller.js
+++ src/app/modules/login/login-controller.js
@@ -58,37 +58,28 @@
 					$scope.password = value;
 				}
 
-				authService.login($scope.username, $scope.password, function (results) {
-                    var result = results[0];
-				if (result.error) {
-					silNoticeService.replace(silNoticeService.ERROR, result.error);
-				} else {
+				authService.login($scope.username, $scope.password, function (result) {
 					silNoticeService.replace(silNoticeService.SUCCESS, "Login Successful");
-
-					// add session token to defaultHeaders
-					authService.setSession(result.sessionToken);
-
 					$state.go('browse'); //we're done here. Go back home
-				}
-			}, function (error) {
-				// catch for login credential failure
-				if (error.status === 404) {
-					if (error.data.code !== undefined) {
-						if (error.data.code === 101) {
-							silNoticeService.replace(silNoticeService.ERROR,
-			"Login Unsuccessful. Check your username and password and try again. Also check the Caps Lock key.");
-						} else {
-							silNoticeService.replace(silNoticeService.ERROR, error);
-						}
-					} else {
-						silNoticeService.replace(silNoticeService.ERROR, error);
-					}
-				} else {
-					silNoticeService.replace(silNoticeService.ERROR, error);
-				}
+                }, function (error) {
+                    // catch for login credential failure
+                    if (error.status === 404) {
+                        if (error.data.code !== undefined) {
+                            if (error.data.code === 101) {
+                                silNoticeService.replace(silNoticeService.ERROR,
+                "Login Unsuccessful. Check your username and password and try again. Also check the Caps Lock key.");
+                            } else {
+                                silNoticeService.replace(silNoticeService.ERROR, error);
+                            }
+                        } else {
+                            silNoticeService.replace(silNoticeService.ERROR, error);
+                        }
+                    } else {
+                        silNoticeService.replace(silNoticeService.ERROR, error);
+                    }
 
-			});
-		};
+                });
+            };
 			$scope.resetPassword = function() {
 				//we're using the email for the account name
 				userService.readByUserName($scope.username, function (result) {
