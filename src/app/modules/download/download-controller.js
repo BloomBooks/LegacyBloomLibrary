e6413121358b90e5193deac30553a2f6a69f2f52
John Hatton <hattonjohn@gmail.com>
2014-07-01 10:47:16 -0600

    More work on downloading

--- src/app/modules/download/download-controller.js
+++ src/app/modules/download/download-controller.js
@@ -4,14 +4,15 @@
     // This module manages the warning page that comes up when a book is first downloaded.
     // its url looks like download/bookId.
     // It's Continue button will attempt to download the specified book.
-    angular.module('BloomLibraryApp.download', ['ui.router', "restangular"])
-        .config(function config($urlRouterProvider, $stateProvider, $compileProvider) {
+    angular.module('BloomLibraryApp.download', ['ui.router'])//, "restangular"])
+        .config(function config($urlRouterProvider, $stateProvider){//}, $compileProvider) {
 
-
+            // REVIEW: is this needed anymore, as we aren't making direct links to the download url?
 
             // Tell angular that urls starting with bloom: are OK. (Otherwise it marks them 'unsafe' and Chrome at
             // least won't follow them.). This is needed for the Continue button.
-            $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|bloom|mailto):/);
+           // $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|bloom|mailto):/);
+
             //we have an abstract parent state that just holds the book id,
             // and then a child for 'preflighting' new users, and another for
             // telling them it is coming.
@@ -21,17 +22,13 @@
                 template: '<ui-view/>',
                 controller: 'DownloadCtrl'
             });
-            $stateProvider.state('downloadBook.begin', {
-                url: "/begin",
-                abstract: false,
-                template: '<ui-view/>',
-                controller: 'DownloadCtrl'
-            });
+            // Give the user a few last words about what they need for a successful download.
             $stateProvider.state('downloadBook.preflight', {
                 url: "/preflight",
                 templateUrl: 'modules/download/preflight.tpl.html',
                 controller: 'DownloadCtrl'
             });
+            // Downloading happens in this state
             $stateProvider.state('downloadBook.hereItComes', {
                 url: "/hereItComes",
                 templateUrl: 'modules/download/hereItComes.tpl.html',
@@ -42,40 +39,9 @@
     angular.module('BloomLibraryApp.download').controller('DownloadCtrl', ['$scope', '$state', '$stateParams','bookService', '$location', 'localStorageService','$rootScope',
 
         function ($scope, $state, $stateParams, bookService, $location, localStorageService, $rootScope) {
-
-
-            // Our state hierarchy has "download", with two children: preflight and hereItComes.
-            // Preflight is used only until users know what they're doing, then they click on a checkbox
-            // that makes it so we don't show that anymore. There must be a more elegant way, with ui-router,
-            // to skip the preflight if we need to. But this it the way I've gotten it to work thusfar:
-            // we intercept the state change to preflight, and if we don't want to go there, then we
-            // instead go straight to the 'hereItComes' screen.
-            $rootScope.$on('$stateChangeStart', function(e, destination) {
-                if (destination.name.indexOf('begin') > -1) {
-                    if(localStorageService.get('skipDownloadPreflight')){
-                        e.preventDefault(); // stop the pending state change
-                        //go straight to the downloading
-                        $state.go('downloadBook.hereItComes').then(function(){
-                            //when that screen is showing, then tell the browser to begin the download
-                            //window.location.href = $scope.book.bookOrder;
-                        });
-                    }
-                }
-            });
-            $rootScope.$on('$stateChangeSuccess', function(e, destination) {
-                if (destination.name.indexOf('hereItComes') > -1) {
-                   // window.location.href = $scope.book.bookOrder;
-                }
-            });
-
             //when the user clicks this checkbox, store their preference
             $scope.$watch('skipDownloadPreflight', function() {
                 localStorageService.set('skipDownloadPreflight',$scope.skipDownloadPreflight);
-            });
-
-            //get the book for which we're going to show the details
-            bookService.getBookById($stateParams.bookId).then(function (book) {
-                $scope.book = book;
             });
 
             $scope.skipDownloadPreflight = localStorageService.get('skipDownloadPreflight');
@@ -87,9 +53,31 @@
             // platform Win32.
             $scope.isWindows = navigator.platform == "Win32";
 
-
             $scope.cancel = function () {
                 $state.go('browse'); //we're done here. Go back home. Review: should we go instead to detail/bookId?
             };
+
+            // Our state hierarchy has "download", with two children: preflight and hereItComes.
+            // Preflight is used only until users know what they're doing, then they click on a checkbox
+            // that makes it so we don't show that anymore. There must be a more elegant way, with ui-router,
+            // to skip the preflight if we need to. But this it the way I've gotten it to work thusfar:
+            // we intercept the state change to preflight, and if we don't want to go there, then we
+            // instead go straight to the 'hereItComes' screen.
+            $scope.$on('$viewContentLoaded',
+                function(event){
+                    localStorageService.set('skipDownloadPreflight', false);
+                    if($state.current.name.indexOf('preflight') > -1 &&
+                        localStorageService.get('skipDownloadPreflight')===true){
+                            event.preventDefault();
+                            $state.go("downloadBook.hereItComes");
+                    }
+                    if($state.current.name.indexOf('hereItComes') > -1){
+                        //get the book for which we're going to show the details asynchronously,
+                        //then start the download
+                        bookService.getBookById($stateParams.bookId).then(function (book) {
+                            window.location.href = book.bookOrder;
+                        });
+                    }
+                });
         } ]);
 } ());  // end wrap-everything function
